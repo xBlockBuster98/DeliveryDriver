349e75d5ef657a12f4c8e58b36f12b1814375e35
xBlockBuster98 <xGH380@gmail.com>
2022-04-19 11:35:44 +0200

    aggiunti assets

--- Assets/scripts/DriverManager.cs
+++ Assets/scripts/DriverManager.cs
@@ -16,8 +16,8 @@ public class DriverManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") * carSpeed;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        float steerAmount = Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Rotate(0, 0, -steerAmount);   
         transform.Translate(0, moveAmount, 0);
